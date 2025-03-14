e2b00a0209279a302902232300e6bc44bed6046e
amir <amirjoun2@gmail.com>
2025-03-14 11:07:22 +0400

    Added for validating profile

---
+++ app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'os' => [
+                'required',
+                Rule::in('linux', 'macos', 'windows')
+            ],
+            'user' => [
+                'required',
+                Rule::in('administrator', 'guest')
+            ],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'success' => $validator->errors(),
+                'message' => $validator->errors(),
+                'data' => false,
+            ], 400)
+        );
+    }
+
+}
